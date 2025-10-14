f025a7272b92a2c2886c1b0a9699ea1d229a197a
Kale-8 <kaled.geme2@gmail.com>
2025-10-14 06:47:38 -0500

    feat: added db connection

---
+++ src/config/database.ts
@@ -0,0 +1,21 @@
+//DB connection
+import dotenv from "dotenv";
+import {Sequelize} from "sequelize";
+
+dotenv.config();
+
+const sqlUri = process.env.SQL_URI as string;
+
+if (!sqlUri) {
+    throw new Error("SQL_URI not defined in env");
+}
+
+export const sequelize = new Sequelize(sqlUri, {
+    logging: false,
+    define: {
+        underscored: true,
+        timestamps: false,
+    },
+});
+
+export default sequelize;
